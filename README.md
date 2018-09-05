aa4ecab492019fcc4baa19c5195f906823cb3492
user <luyaman17th@gmail.com>
2018-09-05 17:50:03 +0800

    Adding class for problem 95

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-Euler
+
+This is just an archive for solving project euler problems.
---
+++ src/Problem/P95.java
@@ -0,0 +1,10 @@
+package Problem;
+
+public class P95 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
