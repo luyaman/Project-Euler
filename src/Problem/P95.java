ef4a537cb5cf482a136668df1beb04f500e51f61
user <luyaman17th@gmail.com>
2018-09-05 18:04:38 +0800

    Problem 95 code added for calculating proper divisor sum.

--- src/Problem/P95.java
+++ src/Problem/P95.java
@@ -1,10 +1,26 @@
 package Problem;
 
+import MathUtil.PrimeUtil;
+
 public class P95 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int limit = 1000000;
+		int[] post = new int[limit];
+		
+		for(int index = 0; index  < limit ; index ++)
+			post[index] = -1;
+		
+		System.out.println("Generating primes...");
+		int[] primes = PrimeUtil.genPrimeList(limit);
+		System.out.println("Generated Primes.");
+		
+		for(int num = 2; num <= limit; num ++){
+			int divisorSum = PrimeUtil.sumProperDivisor(num, primes);
+			System.out.println(String.format("Sum of divisor for %d is %d", num, divisorSum));
+			if(divisorSum <= limit)
+				post[num] = divisorSum;
+		}
 	}
 
 }
